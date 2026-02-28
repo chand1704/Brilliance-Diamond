9ec19d4e27e2fde55bb19ee86ae3b3b6d325f21a
Chand Desai <chanddesai733@gmail.com>
2026-02-28 09:59:17 +0530

    Brilliance- Diamond

--- lib/model/gmss_stone_model.dart
+++ lib/model/gmss_stone_model.dart
@@ -7,9 +7,6 @@ class GmssStone {
   final String colorStr;
   final String clarityStr;
   final String cut;
-  final double length;
-  final double ratio;
-
   final String cut_code;
   final String lab;
   final String fl_intensity;
@@ -19,12 +16,14 @@ class GmssStone {
   final String stoneName;
   final String gridle_condition;
   final String symmetry;
+  final String culet_size;
+  final String? certi_file;
+  final double length;
+  final double ratio;
   final double depth;
   final double width;
   final double table;
-  final String culet_size;
   final double total_price;
-  final String? certi_file;
 
   GmssStone({
     required this.id,
@@ -36,24 +35,22 @@ class GmssStone {
     required this.clarityStr,
     required this.cut,
     required this.cut_code,
-    required this.length,
-    required this.ratio,
-    // this.length = 0.0,
-    // this.ratio = 0.0,
     required this.lab,
     required this.fl_intensity,
     required this.polish,
     required this.image_link,
     required this.video_link,
     required this.stoneName,
-    required this.symmetry,
     required this.gridle_condition,
-    required this.depth,
+    required this.symmetry,
     required this.culet_size,
+    this.certi_file,
+    required this.length,
+    required this.ratio,
+    required this.depth,
     required this.width,
     required this.table,
     required this.total_price,
-    this.certi_file,
   });
 
   factory GmssStone.fromJson(Map<String, dynamic> json) {
@@ -65,6 +62,7 @@ class GmssStone {
 
     String rawShape = json['shape_str']?.toString() ?? 'ROUND';
     String cleanShape = rawShape.split(' ')[0];
+
     String rawVideo = json['video_link']?.toString() ?? "";
     String fullVideo = rawVideo.isEmpty
         ? ""
@@ -81,36 +79,34 @@ class GmssStone {
     }
 
     final shapeItem = json['shape_item'] as Map<String, dynamic>?;
-    // final symmetryItem = json['symmetry_item'] as Map<String, dynamic>?;
+
     final cutItem = json['cut_item'] as Map<String, dynamic>?;
 
     return GmssStone(
       id: json['id'] is int ? json['id'] : 0,
       stockNo: json['stock_no']?.toString() ?? '',
       shapeStr: cleanShape,
-      // shapeStr: json['shape_str']?.toString() ?? '',
       shapeIcon: shapeItem?['image_link']?.toString() ?? '',
-      gridle_condition: json['gridle_condition']?.toString() ?? '',
-      // video_link: json['video_link']?.toString() ?? "",
-      video_link: fullVideo,
+      weight: safeDouble(json['weight']),
       colorStr: json['color_str']?.toString() ?? "",
-      certi_file: fullCert,
       clarityStr: json['clarity_str']?.toString() ?? "",
-      polish: json['polish']?.toString() ?? '',
       cut: json['cut']?.toString() ?? '',
       cut_code: cutItem?['cut_code']?.toString() ?? '',
-      fl_intensity: json['fl_intensity']?.toString() ?? '',
-      image_link: json['image_link']?.toString() ?? '',
-      stoneName: json['stone_name']?.toString() ?? '',
       lab: json['lab_name']?.toString() ?? '',
-      culet_size: json['culet_size']?.toString() ?? '',
+      fl_intensity: json['fl_intensity']?.toString() ?? '',
+      polish: json['polish']?.toString() ?? '',
+      image_link: json['image_link']?.toString() ?? '',
+      video_link: fullVideo,
+      stoneName: json['stone_name']?.toString() ?? '',
+      gridle_condition: json['gridle_condition']?.toString() ?? '',
       symmetry: json['symmetry_item']?['symmetry_code']?.toString() ?? "",
+      culet_size: json['culet_size']?.toString() ?? '',
+      certi_file: fullCert,
+      length: safeDouble(json['length']),
+      ratio: safeDouble(json['ratio']),
       depth: safeDouble(json['depth']),
       width: safeDouble(json['width']),
       table: safeDouble(json['table']),
-      ratio: safeDouble(json['ratio']), // MAP THE RATIO HERE
-      length: safeDouble(json['length']),
-      weight: safeDouble(json['weight']),
       total_price: safeDouble(json['total_price']),
     );
   }
