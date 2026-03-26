743210758314b043e99c223db51bbf78743e0e7d
Chand Desai <chanddesai733@gmail.com>
2026-03-26 16:02:42 +0530

    Brilliance-Diamond

--- lib/main.dart
+++ lib/main.dart
@@ -16,37 +16,21 @@ class MyApp extends StatelessWidget {
       title: 'Brilliance Diamond Store',
       theme: ThemeData(
         useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color(0xFF009688),
-          // primary: const Color(0xFF009688),
-        ),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF009688)),
       ),
       onGenerateRoute: (settings) {
         if (settings.name != null && settings.name!.startsWith('/details')) {
           final uri = Uri.parse(settings.name!);
           final stoneId = uri.queryParameters['id'];
-          // return MaterialPageRoute(builder: (_) => const GmssScreen());
-
-          // return MaterialPageRoute(
-          //   builder: (_) => DiamondDetailScreen(
-          //     stoneId: stoneId,
-          //     isFavorite: false,
-          //     onFavoriteToggle: (val) {},
-          //     stone: null,
-          //   ),
-          // );
           return PageRouteBuilder(
             settings: settings,
             transitionDuration: Duration.zero,
-            // reverseTransitionDuration: Duration.zero,
             pageBuilder: (_, __, ___) => DiamondDetailScreen(
               stoneId: stoneId,
               stone: null,
               isFavorite: false,
               onFavoriteToggle: (val) {},
             ),
-            // transitionsBuilder: (_, animation, __, child) =>
-            //     FadeTransition(opacity: animation, child: child),
           );
         }
         return null;
